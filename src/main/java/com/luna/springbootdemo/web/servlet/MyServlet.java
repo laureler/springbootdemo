165d09ea35a20b5f851c92a5d0186c82ba8e8a0d
yunlong <yunlong.wu@southgis.com>
2020-01-09 18:02:47 +0800

    修改为 异步请求方式

--- src/main/java/com/luna/springbootdemo/web/servlet/MyServlet.java
+++ src/main/java/com/luna/springbootdemo/web/servlet/MyServlet.java
@@ -1,5 +1,6 @@
 package com.luna.springbootdemo.web.servlet;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,12 +11,27 @@ import java.io.IOException;
 /**
  * 配置servlet映射
  */
-@WebServlet(urlPatterns = "/myServlet")
+@WebServlet(urlPatterns = "/myServlet",asyncSupported = true)
 public class MyServlet extends HttpServlet {
 
-    @Override
+    /*@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("hello world");
+    }*/
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 异步上下文
+        AsyncContext asyncContext = req.startAsync();
+        asyncContext.start(()->{
+            try {
+                resp.getWriter().println("hello world");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        // 触发完成事件
+        asyncContext.complete();
+        // resp.getWriter().println("hello world");
     }
 
     @Override
