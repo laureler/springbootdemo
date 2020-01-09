7678497a9c4e5cc7eeef78df7b47f8281d763be1
yunlong <yunlong.wu@southgis.com>
2020-01-09 17:54:49 +0800

    添加serlvet

--- src/main/java/com/luna/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/luna/springbootdemo/SpringbootdemoApplication.java
@@ -2,7 +2,9 @@ package com.luna.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan(basePackages = "com.luna.springbootdemo.web.servlet")
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
